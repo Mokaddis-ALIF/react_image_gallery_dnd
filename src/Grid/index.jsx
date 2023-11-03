d77d60936fb6443cd806344e3363643a7854e10c
Mirazul-Hoq <mirazulhoq807@gmail.com>
2023-11-03 15:30:41 +0600

    drag and drop started

--- src/Grid/index.jsx
+++ src/Grid/index.jsx
@@ -1,14 +1,41 @@
+import { useRef, useState } from 'react'
 import './index.css'
 
 export default function index({ images }) {
+    const [state, setState] = useState(images)
+    const dragItem = useRef()
+    const dragOverItem = useRef()
+
+    const dragStart = (e, position) => {
+        dragItem.current = position
+    };
+
+    const dragEnter = (e, position) => {
+        dragOverItem.current = position
+    };
+
+    const drop = () => {
+        const copyListItems = [...state]
+        const dragItemContent = copyListItems[dragItem.current]
+        copyListItems.splice(dragItem.current, 1)
+        copyListItems.splice(dragOverItem.current, 0, dragItemContent)
+        dragItem.current = null
+        dragOverItem.current = null
+        setState(copyListItems)
+    }
+
     return (
         <div className='image-grid'>
-
-            {images.map((image, index) => (
-
-                <img src={image.url} key={index} className={`${index == 0 ? 'image-grid-col-2 image-grid-row-2' : ''}`} />
-
-
+            {state.map((image, index) => (
+                <img
+                    onDragStart={(e) => dragStart(e, index)}
+                    onDragEnter={(e) => dragEnter(e, index)}
+                    onDragEnd={drop}
+                    draggable
+                    src={image.url}
+                    key={index}
+                    className={`${index == 0 ? 'image-grid-col-2 image-grid-row-2' : ''}`}
+                />
             ))}
         </div>
     )

b0fecf0ab6ce24d61e004ff5feacc3880fe65ff6
Mirazul-Hoq <mirazulhoq807@gmail.com>
2023-11-04 00:19:00 +0600

    drag and drop manually

--- src/Grid/index.jsx
+++ src/Grid/index.jsx
@@ -1,28 +1,29 @@
 import { useRef, useState } from 'react'
 import './index.css'
+import img12 from '../assets/images/image12.jpg'
 
 export default function index({ images }) {
     const [state, setState] = useState(images)
-    const dragItem = useRef()
-    const dragOverItem = useRef()
+    const dragItem = useRef();
+    const dragOverItem = useRef();
 
     const dragStart = (e, position) => {
-        dragItem.current = position
+        dragItem.current = position;
     };
 
     const dragEnter = (e, position) => {
-        dragOverItem.current = position
+        dragOverItem.current = position;
     };
 
-    const drop = () => {
-        const copyListItems = [...state]
-        const dragItemContent = copyListItems[dragItem.current]
-        copyListItems.splice(dragItem.current, 1)
-        copyListItems.splice(dragOverItem.current, 0, dragItemContent)
-        dragItem.current = null
-        dragOverItem.current = null
-        setState(copyListItems)
-    }
+    const drop = (e) => {
+        const copyListItems = [...state];
+        const dragItemContent = copyListItems[dragItem.current];
+        copyListItems.splice(dragItem.current, 1);
+        copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+        dragItem.current = null;
+        dragOverItem.current = null;
+        setState(copyListItems);
+    };
 
     return (
         <div className='image-grid'>

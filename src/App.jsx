6d444a49dac2d9f4a49f5a0d681dd6b218231f08
Mokaddis-ALIF <mokaddis.alif@gmail.com>
2023-11-04 23:53:20 +0600

    dnd

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,29 @@ import img11 from './assets/images/image-11.jpeg'
 import img10 from './assets/images/image-10.jpeg'
 import img7 from './assets/images/image-7.webp'
 import img9 from './assets/images/image-9.webp'
+import { useState } from "react";
 // import img11 from './assets/images/image-11.jpeg'
 
 
 const App = () => {
   const images = [
-    { id: 1, url: img11 },
-    { id: 2, url: img3 },
-    { id: 3, url: img2 },
-    { id: 4, url: img7 },
-    { id: 5, url: img5 },
-    { id: 6, url: img9 },
-    { id: 7, url: img10 },
-    { id: 8, url: img4 },
+    { id: 1, url: img11, checked: false, hover: false },
+    { id: 2, url: img3, checked: false, hover: false },
+    { id: 3, url: img2, checked: false, hover: false },
+    { id: 4, url: img7, checked: false, hover: false },
+    { id: 5, url: img5, checked: false, hover: false },
+    { id: 6, url: img9, checked: false, hover: false },
+    { id: 7, url: img10, checked: false, hover: false },
+    { id: 8, url: img4, checked: false, hover: false },
 
     // Add more images here
   ];
+
+  const [state, setState] = useState(images)
+  console.log(state);
+
+
+  // const selectedFiles = state.filter(item => item.checked == true)
 
   return (
     <div className="app">
@@ -31,10 +38,15 @@ const App = () => {
       <ImageGallery images={images} /> */}
       <div className="main">
         <div className="header">
-          <div> <span>0 files selected</span></div>
+          <div>
+            {/* <label className="container">
+              <input type="checkbox" checked={selectedFiles.length == 0 ? false : true} ></input>
+              <span className="checkmark"></span>
+            </label> */}
+            <span>{ } files selected</span></div>
           <span>Delete files</span>
         </div>
-        <Grid images={images} />
+        <Grid state={state} setState={setState} />
       </div>
     </div>
   );
--- src/Grid/index.jsx
+++ src/Grid/index.jsx
@@ -2,16 +2,17 @@ import { useRef, useState } from 'react'
 import './index.css'
 import img12 from '../assets/images/image12.jpg'
 
-export default function index({ images }) {
-    const [state, setState] = useState(images)
+export default function index({ state, setState }) {
     const dragItem = useRef();
     const dragOverItem = useRef();
 
     const dragStart = (e, position) => {
+
         dragItem.current = position;
     };
 
     const dragEnter = (e, position) => {
+
         dragOverItem.current = position;
     };
 
@@ -25,26 +26,84 @@ export default function index({ images }) {
         setState(copyListItems);
     };
 
+    const checkedHandler = (e, id) => {
+        console.log(id);
+        const imageIndex = state.findIndex(item => item.id == id);
+        const imageUpdate = state.find(item => item.id == id);
+        // console.log(imageUpdate);
+        const copyListItems = [...state];
+        copyListItems[imageIndex] = { ...imageUpdate, checked: !imageUpdate.checked }
+        // console.log(copyListItems);
+        setState(copyListItems)
+    }
+
+
+
+    // const dynamicStyle = {
+    //     backgroundColor: isHovered ? 'blue' : '',
+    //     color: 'white',
+    //     padding: '10px',
+    // };
+
+    const mouseEnterHandler = (id) => {
+        console.log(id);
+        const imageIndex = state.findIndex(item => item.id == id);
+        const imageUpdate = state.find(item => item.id == id);
+        // console.log(imageUpdate);
+        const copyListItems = [...state];
+        copyListItems[imageIndex] = { ...imageUpdate, hover: true }
+        // console.log(copyListItems);
+        // return
+        setState(copyListItems)
+
+    }
+
+    const mouseLeaveHandler = (id) => {
+        console.log(id);
+        const imageIndex = state.findIndex(item => item.id == id);
+        const imageUpdate = state.find(item => item.id == id);
+        // console.log(imageUpdate);
+        const copyListItems = [...state];
+        copyListItems[imageIndex] = { ...imageUpdate, hover: false }
+        // console.log(copyListItems);
+        // return
+
+        setState(copyListItems)
+
+    }
+
     return (
         <div className='image-grid'>
             {state.map((image, index) => (
                 <div
-                    className={`${index == 0 ? 'image-grid-col-2 image-grid-row-2' : ''}`}
-                    key={index}>
+                    className={`${index == 0 ? 'image-grid-col-2 image-grid-row-2' : ''} main-div`}
+                    key={index}
 
 
-                    <label className="container">One
-                        <input type="checkbox" checked="checked"></input>
+                >
+
+
+                    <label className="container" >
+                        <input
+
+                            type="checkbox" checked={image.checked} onChange={e => checkedHandler(e, image.id)}></input>
                         <span className="checkmark"></span>
                     </label>
-
                     <img
                         onDragStart={(e) => dragStart(e, index)}
                         onDragEnter={(e) => dragEnter(e, index)}
                         onDragEnd={drop}
-                        draggable
+                        draggable={image.checked == true ? true : false}
                         src={image.url}
                         className='grid-img'
+                    // onMouseEnter={() => mouseEnterHandler(image.id)}
+                    // onMouseLeave={() => mouseLeaveHandler(image.id)}
+                    // style={dynamicStyle}
+                    // style={{
+                    //     // backgroundColor: image.hover == true ? '#808080' : '',
+                    //     // opacity: image.hover == true ? 0.5 : '',
+                    //     // transition: image.hover == true ? 'opacity 0.25s' : ''
+                    // }}
                     />
                 </div>
             ))}
